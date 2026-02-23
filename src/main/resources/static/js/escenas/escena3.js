57237c01fb4d9e63c41de8db38a55c828b177076
Mijail <mijail42106@gmail.com>
2026-02-23 17:44:11 -0300

    Co-authored-by: LucasBichi <LucasBichi@users.noreply.github.com>

--- src/main/resources/static/js/escenas/escena3.js
+++ src/main/resources/static/js/escenas/escena3.js
@@ -17,7 +17,8 @@ gameState = {
     portaNY: 35,
     portaAX: 63,
     portaAY: 0,
-    escala: 22.36
+    escala: 22.36,
+    tamCelda: 23
 }; 
 
 const mensaje = {
@@ -33,7 +34,7 @@ class Celda {                                   // calse celda para grilla
     constructor (grid, y, x) {                  // grid = escena donde se crean, indices para posiciones x e y
         gameState.escala = 22.36;                       // escala de posiciones
                                                 // añade rectangulo en posicion correspondiente a indices
-        this.tile = grid.add.rectangle(x*gameState.escala, y*gameState.escala, 23, 23, gameState.niebla).setStrokeStyle(1, gameState.bordes);
+        this.tile = grid.add.rectangle(x*gameState.escala, y*gameState.escala, gameState.tamCelda, gameState.tamCelda, gameState.niebla).setStrokeStyle(1, gameState.bordes);
         this.tile.setAlpha(0.6);                // ajuste de opacidad para celdas de grilla
         this.tile.setInteractive();             // se setea interactivo para poder darle interaccion con mouse despues
                                                 // 
@@ -61,15 +62,10 @@ class Celda {                                   // calse celda para grilla
         });
         
         this.tile.on('pointerover', () => {
-            //this.tile.setStrokeStyle(3, 0x00ff3cc1);
             grid.indx.setText("X: "+x);
             grid.indy.setText("Y: "+y);
         });
         
-        //this.tile.on('pointerout', () => {
-        //    this.tile.setStrokeStyle(1, 0x9398c36e);
-        //});
-
         grid.tablero.add(this.tile);            // agrega el rectangulo creado a el tablero
     }
 }   
@@ -100,43 +96,27 @@ class escena3 extends Phaser.Scene {
     }
 
     create() {
-        this.graphics = this.add.graphics();
+        //this.graphics = this.add.graphics();
         this.crearFondo();
+        this.crearAnimaciones();
 
-        const portadron = this.add.image(gameState.portaNX*gameState.escala + gameState.tableroX + 11, gameState.portaNY*gameState.escala + gameState.tableroY-27,"PortaN").setDepth(2).setOrigin(0.5, 0.5);
-        portadron.setScale(1.5);
+        const portadronN = this.add.image(gameState.portaNX*gameState.escala + gameState.tableroX + 11, gameState.portaNY*gameState.escala + gameState.tableroY-27,"PortaN").setDepth(2).setOrigin(0.5, 0.5);
+        portadronN.setScale(1.5);
+        const portadronA = this.add.image(gameState.portaAX*gameState.escala + gameState.tableroX - 11, gameState.portaAY*gameState.escala + gameState.tableroY+27,"PortaA").setDepth(2).setOrigin(0.5, 0.5);
+        portadronA.setScale(1.5);
+        portadronA.angle = 180;
 
-        this.forma = this.add.graphics();
-        //this.forma.fillStyle(0xa4cbcb52);
+        this.forma = this.add.graphics().setDepth(3);
         this.forma.clear();
+        this.forma.fillStyle(0xffffff);
         this.mask = this.forma.createGeometryMask();
-        //forma.beginPath();    
-        //forma.fillRect(0,0, 50, 50)
-        //this.add.rectangle(10, 10, 50, 50, gameState.niebla);
 
-        
-        
-
-        portadron.setMask(this.mask);
-
-        //this.input.on('pointermove', function (pointer) {
-        //    forma.x = pointer.x - 25;
-        //    forma.y = pointer.y - 25;
-//
-        //});
-        
-
-        //forma.clear()
-        //portadron.setAlpha(0);
-        //portadron.clearMask();
-
-        
-        //this.pasarBtn.destroy();
+        portadronN.setMask(this.mask);
+        portadronA.setMask(this.mask);
 
         let indice = 0;                                 // indice para pruebas de posiciones de modifciaciones de celdas
                                                         // crecion de textos de control de variables y eventos
         
-
         let prueba = this.add.text(1500 , 900,"ii: "+ mensaje.nombre, { fill: "#222222", font: "40px Times New Roman"});
 
         this.indiceprueba = this.add.text(1700 , 800,"msg: ", { fill: "#222222", font: "40px Times New Roman"});
@@ -147,7 +127,7 @@ class escena3 extends Phaser.Scene {
 
         this.drs = this.add.text(1500 , 700,"drs: " , { fill: "#222222", font: "40px Times New Roman"});
                                                         
-        // Conexión STOMP con SockJS
+        // conexion STOMP con SockJS
         this.conectarSTOMP();
 
         this.tablero = this.add.container (gameState.tableroX, gameState.tableroY);     // creaccion de elemento container que almacenara las celdas 
@@ -159,28 +139,23 @@ class escena3 extends Phaser.Scene {
         } 
     }
 
-    /**
-     * Establece la conexión STOMP usando SockJS
-     */
+    // establece conexion STOMP con SockJS
     conectarSTOMP() {
-        // Crear socket con SockJS
-        const socket = new SockJS('http://26.169.248.78:8080/game'); // Para producción: window.location.origin + '/game'
+        // conexion socket con SockJS
+        const socket = new SockJS('http://26.169.248.78:8080/game'); 
         this.stompClient = Stomp.over(socket);
         
-        this.stompClient.debug = null;// Deshabilitar logs de debug
+        this.stompClient.debug = null;// deshabilita logs de debug
         
-        // Conectar al servidor STOMP
+        // conectar STOMP
         this.stompClient.connect({}, (frame) => {
-            // Suscribirse al topic /topic/game para recibir actualizaciones
+            // suscribirse a topic /topic/game
             this.stompClient.subscribe('/topic/game', (message) => {
                 var msg = JSON.parse(message.body);
                 
-                // Actualizar UI con el estado recibido
-                //if (this.indiceprueba) {
                 this.indiceprueba.setText("msg: " + msg.tipoMensaje);
-                //}
                 
-                // Procesar mensaje según su tipo
+                // procesar mensaje
                 switch (msg.tipoMensaje) {
                     case 0: {
                         if (mensaje.nombre === msg.nombre) {
@@ -196,45 +171,33 @@ class escena3 extends Phaser.Scene {
                             gameState.fase = msg.fasePartida.toString();
                         }
                         this.pruebasi.setText("faselocal: " + gameState.fase);
-                        //portadron.clearMask();
-                        //forma.closePath();  //  //
-                        this.forma.clear();      //  //
-                        //forma.beginPath();  //  //
+                        this.forma.clear(); 
+                        this.forma.fillStyle(0xff0000, 0);
                         this.eliminarDrones();
                         var i = 0;
                         msg.grilla.forEach((cel) => {
                             let celda = this.tablero.getAt(i);
                             if (!cel.visionNaval && gameState.equipo === "NAVAL") {
-                                celda.setFillStyle(0x334455);
+                                celda.setFillStyle(gameState.niebla);
                             } else if (!cel.visionAereo && gameState.equipo === "AEREO") {
-                                celda.setFillStyle(0x334455);
+                                celda.setFillStyle(gameState.niebla);
                             } else if (cel.aereo !== null) {
                                 if (gameState.equipo === "AEREO" || cel.visionNaval) {
-                                    celda.setFillStyle(gameState.colorRojo);
                                     this.dibujarDronAereo(celda.x, celda.y);
                                 }
                             } else if (cel.naval !== null) {
                                 if (gameState.equipo === "NAVAL" || cel.visionAereo) {
-                                    celda.setFillStyle(gameState.colorVerde);
                                     this.dibujarDronNaval(celda.x, celda.y);
                                 }
                             } else {
                                 celda.setFillStyle(0xffffff);
 
-                                if( (gameState.equipo === "AEREO" && cel.visionNaval) || (gameState.equipo === "NAVAL" && cel.visionAereo) ){
-                                    forma.fillRect(celda.x+gameState.tableroX-11,celda.y+gameState.tableroY-11, 23, 23);
-                                }
-                                
-                                //if (gameState.equipo === "AEREO" && celda.x >= gameState.portaNX && celda.x <= gameState.portaNX+1 && celda.y >= gameState.portaNY-2 && y <= gameState.portaNY) {
-                                //    forma.fillRect(celda.x+gameState.tableroX,celda.y+gameState.tableroY, 23, 23);
-                                //}
-                                //else if () {  (x >= xPorta-1 && x <= xPorta && y >= yPorta && y <= yPorta+2)    // Portaaviones aéreo
-                                //}
-                                
+                                if( (celda.x <= 1*gameState.tamCelda && celda.y >= 32*gameState.tamCelda) || (celda.x >= 60*gameState.tamCelda && celda.y <= 2*gameState.tamCelda) )  {
+                                        this.forma.fillRect(celda.x + gameState.tableroX -11, celda.y + gameState.tableroY -11, gameState.tamCelda, gameState.tamCelda);
+                                }                           
                             }
                             i++;
                         });
-                        //portadron.setMask(mask);
                     } break;
                     case 2: {
                         if (mensaje.nombre === msg.nombre) { // alerta error
@@ -251,11 +214,22 @@ class escena3 extends Phaser.Scene {
             setTimeout(() => this.conectarSTOMP(), 5000);
         });*/
     }
-    /*
-    pintarCelda(indice1){                               // pinta celda 
-        let celda1 = this.tablero.getAt(indice1);       // puede hacerse en una linea y sin aux, no probado
-        celda1.setTint(0xff44ff);
-    }*/
+
+    crearAnimaciones(){
+        this.anims.create({
+            key: 'idleN',
+            frames: this.anims.generateFrameNumbers('DronN', { start: 0, end: 1 }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: 'idleA',
+            frames: this.anims.generateFrameNumbers('DronA', { start: 0, end: 1 }),
+            frameRate: 10,
+            repeat: -1,
+        });
+    }
 
     botonPasar(boton) {
         const p = boton;
@@ -376,6 +350,7 @@ class escena3 extends Phaser.Scene {
         var yAbs = y + gameState.tableroY;
         let dron = this.add.sprite(xAbs , yAbs ,"DronN").setScale(1.5).setDepth(2);
         dron.angle = -90;
+        dron.play('idleN');
         gameState.drones.push(dron);
     }
 
@@ -384,6 +359,7 @@ class escena3 extends Phaser.Scene {
         var yAbs = y + gameState.tableroY;
         let dron = this.add.sprite(xAbs, yAbs ,"DronA").setScale(1.5).setDepth(2);
         dron.angle = 90;
+        dron.play('idleA');
         gameState.drones.push(dron);
     }
 
@@ -395,24 +371,16 @@ class escena3 extends Phaser.Scene {
         gameState.drones.length = 0;
     }
 
-    /**
-     * Envía un mensaje al servidor mediante STOMP
-     * @param {string} data - Datos en formato JSON string a enviar
-     */
     enviarMensage(data) {
         if (this.stompClient && this.stompClient.connected) {
-            // Enviar mensaje a /app/accion (el servidor lo recibe en @MessageMapping("/accion"))
-            //console.log("ENVIANDO al servidor:", data);
+            // enviar mensaje a /app/accion, servidor recibe en @MessageMapping("/accion")
             this.stompClient.send("/app/accion", {}, data);
-            //console.log("Mensaje enviado exitosamente");
         } else {
-            console.error('Cliente STOMP no está conectado');
+            console.error('Cliente STOMP no conectado');
         }
     }
 
-    /**
-     * Cierra la conexión STOMP al salir de la escena
-     */
+    // cierra conexion STOMP al salir
     apagar() {
         if (this.stompClient && this.stompClient.connected) {
             this.stompClient.disconnect(() => {
